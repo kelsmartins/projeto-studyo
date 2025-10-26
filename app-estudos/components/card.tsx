71af2f25335af41368865d14c1723a346378f626
klss <kelsoglauber11@gmail.com>
2025-10-26 17:33:57 -0400

    ajuste nos cards e criação de card detalhado

--- app-estudos/components/card.tsx
+++ app-estudos/components/card.tsx
@@ -1,7 +1,11 @@
 export function Card() {
     return (
-        <div className="h-[150px] w-[300px] bg-yellow-600 rounded-lg flex-shrink-0">
-            
+        <div className="h-[150px] w-[300px] rounded-lg flex-shrink-0 bg-zinc-800 flex">
+            <span className="bg-red-500 w-[20px] h-full rounded-l-lg"></span>
+            <div className="w-full rounded-r-lg flex flex-col p-4">
+                <h3>21/10/2025</h3>
+                <h2>Matemática: Funções</h2>
+            </div>
         </div>
     )
 }
---
+++ app-estudos/components/carddetalhado.tsx
@@ -0,0 +1,14 @@
+export function CardDetalhado(){
+    return (
+        <div className="w-[350px] h-[400px] bg-red-200 flex flex-col p-4">
+            <div className="w-full h-[70px] flex flex-col items-center justify-center overflow-hidden px-4">
+                <h2 className="text-center">Matemática: funções aaa aaa aaaa aaa aaaaa aaaaa aaaaa aaaa aaaaaa a</h2>
+                <h2>25/10/2025</h2>
+            </div>
+            <div className="w-full h-[200px] bg-green-200 flex flex-col">
+                <h3>Fontes digitais</h3>
+                <div className=""></div>
+            </div>
+        </div>
+    )
+}
