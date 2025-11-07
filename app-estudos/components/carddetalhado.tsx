609051b5ee6f11f8df796c4ffed51a2879c00798
klss <kelsoglauber11@gmail.com>
2025-11-06 23:51:53 -0400

    fechar carddetalhado ao clicar no fundo

--- app-estudos/components/carddetalhado.tsx
+++ app-estudos/components/carddetalhado.tsx
@@ -10,10 +10,10 @@ type Props = {
 export function CardDetalhado({assuntoData, onClose}: Props) {
 
     return (
-        <div className="w-full h-full bg-black/60 fixed inset-0 flex items-center justify-center z-50">
+        <div className="w-full h-full bg-black/60 fixed inset-0 flex items-center justify-center z-50" onClick={onClose}>
             <div className="w-[350px] h-[400px] bg-zinc-800 flex flex-col p-4 rounded-lg">
                 <div className="w-full h-[70px] flex flex-col items-center justify-center">
-                    <h2 className="w-full h-[40px] text-base flex items-center justify-center font-bold">{assuntoData.assunto}</h2> {/* 38 caracteres*/}
+                    <h2 className="w-full h-[40px] text-base flex items-center justify-center font-bold text-center">{assuntoData.assunto}</h2> {/* 38 caracteres*/}
                     <h2 className="w-full h-[15px] text-base flex items-center justify-center font-bold text-white/60 mb-4">{new Date(assuntoData.dataAgendada).toLocaleDateString()}</h2>
                 </div>
                 <div className="w-full h-[200px] flex flex-col">
