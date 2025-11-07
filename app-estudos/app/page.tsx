e58afb1ac010b5bcbc04ca0ae5ecec0f45ee5c76
klss <kelsoglauber11@gmail.com>
2025-11-07 00:10:48 -0400

    titulo homepage

--- app-estudos/app/page.tsx
+++ app-estudos/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
       </div>                                               {/* O min-w-0 permite que um item (flex-1) encolha para caber no layout, mesmo que o conteúdo dentro dele seja muito mais largo. */}
       <div className="h-screen w-[100%] flex-1 bg-zinc-900 min-w-0">
         <div className="h-[10%] w-[100%] flex flex-row items-center px-4 justify-between">
-          <h2 className="text-3xl">Estudos</h2>
+          <h2 className="text-3xl font-bold">Estudos</h2>
           <button className="w-[50px] h-[50px] bg-red-200">oi</button>
         </div>                      
          <div className=" w-full h-[90%] overflow-y-auto no-scrollbar">
