5100fba0ee54e67efe4c6f4c65ffb2a19a5a882e
klss <kelsoglauber11@gmail.com>
2025-11-08 02:59:02 -0400

    botao+funcaoAddNewCard (precisa implementar a logica da função)

--- app-estudos/app/page.tsx
+++ app-estudos/app/page.tsx
@@ -16,6 +16,10 @@ export default function Home() {
   function handleCloseNewCard(){
     setNewCardVisible(false);
   }
+
+  function handleSaveNewCard(){
+    alert('salvo');
+  }
   
   return (
     <div className="w-screen h-screen flex overflow-hidden">
@@ -25,7 +29,8 @@ export default function Home() {
       <div className="h-screen w-[100%] flex-1 bg-zinc-900 min-w-0">
         <div className="h-[10%] w-[100%] flex flex-row items-center px-4 justify-between">
           <h2 className="text-3xl font-bold">Estudos</h2>
-          <button className='w-[110px] h-[40px] border-1 border-zinc-700 rounded-md font-bold p-1 text-sm text-zinc-400 hover:bg-zinc-700 transition-colors' onClick={handleOpenNewCard}>novo assunto</button>
+          <button className='w-[110px] h-[40px] border-1 border-zinc-700 rounded-md font-bold p-1 text-sm text-zinc-400 hover:bg-zinc-700 transition-colors' 
+          onClick={handleOpenNewCard}>novo assunto</button>
         </div>                      
          <div className=" w-full h-[90%] overflow-y-auto no-scrollbar">
             <CardArea title='Hoje'code={1}/>
@@ -35,7 +40,7 @@ export default function Home() {
         </div>
       </div>
       {newCardVisible &&
-        <NovoCard onClick={handleCloseNewCard}/>}
+        <NovoCard onClick={handleCloseNewCard} onConcluir={handleSaveNewCard}/>}
     </div>
   );
 }
--- app-estudos/components/novocard.tsx
+++ app-estudos/components/novocard.tsx
@@ -3,19 +3,30 @@ import { Button } from "./button"
 
 type Props = {
     onClick: () => void;
+    onConcluir: () => void;
 }
 
-export function NovoCard({onClick}: Props) {
+export function NovoCard({onClick, onConcluir}: Props) {
+
+    const [title, setTitle] = useState('');
+    const [link, setLink] = useState('');
+
+    function handleOnConcluir(){
+        onConcluir();
+        onClick();  
+    }
 
     return (
         <div className="w-full h-full bg-black/30 fixed inset-0 flex items-center justify-center z-50 backdrop-blur-sm" onClick={onClick}>
-            <div className="w-[350px] h-[500px] bg-zinc-800 flex flex-col p-4 rounded-lg">
+            <div className="w-[350px] h-[500px] bg-zinc-800 flex flex-col p-4 rounded-lg" onClick={e => e.stopPropagation()}> {/* explicacao linha 52 */}
 
                 <h2 className="w-full h-[35px] text-base flex items-center justify-center font-bold text-center uppercase">novo assunto</h2> {/* 38 caracteres*/}
 
                 <h3 className="mb-1 text-sm uppercase">título</h3>        
                 <input type="text"
                 className="h-[40px] w-full border-1 border-zinc-700 rounded-md p-2 text-white text-sm mb-3 placeholder-italic"
+                value={title}
+                onChange={text => setTitle(text.target.value)}
                 />
 
                 <h3 className="mb-1 text-sm uppercase">data agendada</h3>
@@ -25,11 +36,22 @@ export function NovoCard({onClick}: Props) {
                 {/* <div className="h-[60px] w-full border-1 border-zinc-700 rounded-md p-2 text-white text-sm mb-3"></div> */}
 
                  <h3 className="mb-1 text-sm uppercase">link (Site ou YouTube)</h3>           
-                <div className="h-[40px] w-full border-1 border-zinc-700 rounded-md p-2 text-white text-sm mb-3"></div>
+                <input type="text"
+                 className="h-[40px] w-full border-1 border-zinc-700 rounded-md p-2 text-white text-sm mb-3"
+                 value={link}
+                onChange={text => setLink(text.target.value)}
+                />
 
-                <Button style="bg-red-400 m-auto h-[40px] w-[150px]" title="concluir" onClick={onClick}/>
+                <Button style="bg-red-400 m-auto h-[40px] w-[150px]" title="concluir" onClick={handleOnConcluir}/>
 
             </div>
         </div>
     )
 }
+
+/*  
+    STOPPROPAGATION É TIPO:
+
+        "Ei, o clique aconteceu aqui neste card. Execute esta função (stopPropagation) e pare aqui.
+        Não 'borbulhe' este evento de clique para nenhum componente pai."
+*/  
