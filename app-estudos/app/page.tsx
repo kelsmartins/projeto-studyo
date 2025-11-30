68c0665015155180ecf6edf82ccc4e71261cbb8a
klss <kelsoglauber11@gmail.com>
2025-11-29 21:06:48 -0400

    Update page.tsx

--- app-estudos/app/page.tsx
+++ app-estudos/app/page.tsx
@@ -35,44 +35,6 @@ export default function Home() {
     }
 
     setAssuntos(prev => [...prev, cardTest])
-
-  //     // se enviou arquivo  (INICIO)  
-  //     const uploadedUrls: string[] = []; // teste
-
-  //     if(files){ // se existir arquivo
-  //       for (const file of files) { // PARA CADA FILE EM FILES, FACA REQUISICAO  CLOUDINARY SO ACEITA UM ARQUIVO POR REQUISIÇÃO 
-  //         const formData = new FormData();
-  //           formData.append('file', file);
-  //           formData.append('upload_preset', 'primeiropreset');
-  //                                                                   // minha url no cloudinary dil7jwa49
-  //         const fileRes = await axios.post('https://api.cloudinary.com/v1_1/dil7jwa49/images/upload', // enviar somente imagem e devolver url
-  //         formData                                                                               // para salvar no banco de dados
-  //         );
-  //         // alert('url arquivo: '+ fileRes.data.secure_url); TEST TEST TEST TEST TEST
-  //         uploadedUrls.push(fileRes.data.secure_url); // teste: adiciona urls em cada posicao do vetor se tiver
-  //       }
-  //     }
-  //     // se EXISTIREM ARQUIVOS (FIM)
-
-  //     // REQUISICAO PRA API DO BACKEND
-
-  //     try {
-        
-  //       const newId = crypto.randomUUID().toString();
-
-  //       const res = axios.post('', {
-  //         id: newId,
-  //         dataAgendada: date,
-  //         assunto: title,
-  //         categoria: category,
-  //         cor: color,
-  //         notasRapidas: quickNotes,
-  //         linkFonteDigital: links,
-  //         arquivosFonteDigital: uploadedUrls
-  //       })
-  //     } catch (error) {
-  //       console.log(error)
-  //     }
   }
   
   return (
